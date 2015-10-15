7be817cf3ca0609da7148327e2a7954689af33ec
jamiesun <jamiesun.net@gmail.com>
2015-10-15 12:43:42 +0800

    docker dir change

--- centos-install.sh
+++ centos-install.sh
@@ -46,6 +46,6 @@ sysctl -w net.ipv4.ip_forward=1
 # configure firewall
 iptables -t nat -A POSTROUTING -s 10.79.97.0/24 -o eth0 -j MASQUERADE
 iptables -A FORWARD -s 10.79.97.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
-
+iptables save
 
 exec "$@"
