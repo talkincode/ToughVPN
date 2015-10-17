417d67ff288d630a0ca18a13b66f994d389cea3c
jamiesun <jamiesun.net@gmail.com>
2015-10-18 02:46:39 +0800

    docker dir change

--- centos-install.sh
+++ centos-install.sh
@@ -10,6 +10,8 @@ RADIUS_ACCT_PORT=1813
 yum update -y
 yum install -y pptpd iptables gcc make
 
+CDIR="`pwd`"
+
 
 cd /usr/local/src &&\
   wget ftp://ftp.freeradius.org/pub/freeradius/freeradius-client-1.1.7.tar.gz && \
@@ -20,6 +22,7 @@ cd /usr/local/src &&\
 
 # setup pptp
 
+cd $CDIR
 cat pptp/pptpd.conf > /etc/pptpd.conf
 cat pptp/ppp/pptpd-options > /etc/ppp/pptpd-options
 
@@ -44,7 +47,7 @@ sed -i "s/RADIUS_ADDR:RADIUS_ACCT_PORT/$RADIUS_ADDR:$RADIUS_ACCT_PORT/g" /etc/ra
 sysctl -w net.ipv4.ip_forward=1
 
 # configure firewall
-local_ip = "`ifconfig eth0 | grep "inet addr" | awk '{print $2}' |tr -d "addr:"`"
+local_ip="`ifconfig eth0 | grep "inet addr" | awk '{print $2}' |tr -d "addr:"`"
 iptables -t nat -A POSTROUTING -s 10.79.97.0/255.255.0.0 -o eth0 -j MASQUERADE
 iptables -t nat -A POSTROUTING -s 10.79.97.0/255.255.0.0 -o eth0 -j SNAT --to-source ${local_ip}
 iptables -A FORWARD -s 10.79.97.0/255.255.0.0 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
